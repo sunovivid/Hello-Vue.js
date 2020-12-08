1a74d47f3d323768cf9db60832b47ebd3aabfad2
sunovivid <suno.vivid@gmail.com>
2020-12-08 20:08:03 +0900

    Test Vue features

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 var app = new Vue({ 
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        seen: false,
+        todos: [
+            { text: 'JS배우기' },
+            { text: 'Vue배우기' },
+            { text: '12월까지 사이트 만들기' }
+        ]
+    },
+    methods: {
+        reverseMessage: function() {
+            this.message = this.message.split('').reverse().join('');
+        }
     }
 });
